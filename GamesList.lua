a657f7082b37be0c76b50dce50ff666ccd8ef482
slimtrix103125 <adenarias556@gmail.com>
2026-02-16 03:21:29 -0800

    Update and rename GamesList.lua to GamesList

--- GamesList.lua
+++
@@ -1,5 +0,0 @@
-getgenv().Games = {
-    [131623223084840] = loadstring(
-        game:HttpGet("https://raw.githubusercontent.com/slimtrix103125/SlimTrix-Development-Scripts/main/EscapeTsunamiForBrainrots.lua")
-    )()
-}
