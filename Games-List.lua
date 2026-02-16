baff01679cb23502a55905754dd57fede358fa16
slimtrix103125 <adenarias556@gmail.com>
2026-02-16 02:44:46 -0800

    Update Games-List.lua

--- Games-List.lua
+++ Games-List.lua
@@ -1,3 +1,3 @@
 getgenv().Games = {
-  [131623223084840] = loadstring(game:HttpGet("https://raw.githubusercontent.com/slimtrix103125/SlimTrix-Development-Scripts/Scripts/Escape-Tsunami-For-Brainrots.lua"))(), -- Escape Tsunami For Brainrots
+  [131623223084840] = loadstring(game:HttpGet("https://raw.githubusercontent.com/slimtrix103125/SlimTrix-Development-Scripts/Scripts/Escape-Tsunami-For-Brainrots.lua"))() , -- Escape Tsunami For Brainrots
 }
